6d87b52a91ddcd3897eff31180909fd29143b6a8
Danny August Ramaputra <august_danny@yahoo.co.id>
2019-06-07 06:07:16 +0700

    01_gym_cartpole (#2)
    * created cartpole runner
    
    * created general use ff neural net with genetic alg
    
    * allowed different activation func per layer
    
    * added more activation functions, refactored files for better notebook integration

---
+++ helper/setup.sh
@@ -0,0 +1,9 @@
+pip install pyvirtualdisplay > /dev/null 2>&1
+apt-get install -y xvfb python-opengl ffmpeg > /dev/null 2>&1
+
+apt-get update > /dev/null 2>&1
+apt-get install cmake > /dev/null 2>&1
+pip install --upgrade setuptools 2>&1
+pip install ez_setup > /dev/null 2>&1
+
+pip install -r ../requirements.txt > /dev/null 2>&1
