cc8d6bcc08f31b0aa87ccde16083eca1797abe6d
Danny August Ramaputra <augst_danny@yahoo.co.id>
2019-06-08 11:27:56 +0700

    added srcdump helper

---
+++ helper/srcdump.py
@@ -0,0 +1,13 @@
+import IPython
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
+def dump(filename):
+    with open(filename) as f:
+        code = f.read()
+
+    formatter = HtmlFormatter()
+    IPython.display.HTML('<style type="text/css">{}</style>{}'.format(
+        formatter.get_style_defs('.highlight'),
+        highlight(code, PythonLexer(), formatter)))
